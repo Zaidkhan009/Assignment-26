c676c06fa60e02997f7f0f7a6736f732cb2496b4
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-21 01:44:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+let allien_colour = "green";
+if (allien_colour == "green") {
+    console.log("The player just earned 5 point for shooting the allien ");
+}
+else {
+    console.log("The player just earned 10 points");
+}
+allien_colour = "red";
+if (allien_colour == "green") {
+    console.log("The player just earned 5 point for shooting the allien ");
+}
---
+++ app.ts
@@ -0,0 +1,15 @@
+// let allien_colour: string ="green";
+
+
+// if(allien_colour  == "green"){
+//     console.log("The player just earned 5 point for shooting the allien ");
+// }
+
+// else{
+//     console.log("The player just earned 10 points")
+// }
+
+// allien_colour = "red";
+//     if(allien_colour  == "green"){
+//         console.log("The player just earned 5 point for shooting the allien ");
+// }
